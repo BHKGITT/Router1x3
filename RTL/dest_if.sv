afafe044cc4a00a2e3a28d673935ca34711f7184
Hari Krishna <158833808+BHKGITT@users.noreply.github.com>
2024-02-05 10:26:54 +0530

    Router1x3

--- RTL/dest_if.sv
+++ RTL/dest_if.sv
@@ -1,1 +1,24 @@
+/////////////////////////// DESTINATION_INTERFACE //////////////////////////////
+interface dest_if(input bit clock);
+	logic [7:0] data_out;
+	logic	    vld_out;
+	logic	    read_enb;
+clocking dst_drv_cb@(posedge clock);
+	default input #1 output #1;
+	output	read_enb;
+	input	vld_out;
+endclocking
+
+clocking dst_mon_cb@(posedge clock);
+	default input #1 output #1;
+	input read_enb;
+	input data_out;
+endclocking
+
+modport DDRV_MP	(clocking dst_drv_cb);
+modport DMON_MP	(clocking dst_mon_cb);
+
+endinterface:dest_if
+
+
 
